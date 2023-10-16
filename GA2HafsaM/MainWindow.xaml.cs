e1702eafdd53b76994cdade99f156614cfb04e13
hyktv <hafsa.yktv@gmail.com>
2023-10-16 13:22:10 -0700

    created the calculator part

--- GA2HafsaM/MainWindow.xaml.cs
+++ GA2HafsaM/MainWindow.xaml.cs
@@ -25,17 +25,65 @@ namespace GA2HafsaM
             InitializeComponent();
         }
 
-        //Display Full Name
+        //Display Full Name - Message Box
         private void btnFullName_Click(object sender, RoutedEventArgs e)
         {
+            //create string variables
             string firstName = txtFirstName.Text;
             string lastName = txtLastName.Text;
 
+            //Combine string variable
             string fullName = $"{firstName} {lastName}";
 
+            //put into messagebox for result
             MessageBox.Show(fullName);
+
         } //btnFullName_Click
 
 
+          
+        //Perform a Calculation - Label
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            //create string variables
+            string number1= txtNumber1.Text;
+            string number2= txtNumber2.Text;
+
+            //create new variables to parse to make it into math 
+            int num1 = int.Parse(number1);
+            int num2 = int.Parse(number2);
+
+            //create signs for readablity
+            string sign = "+";
+            string equalSign = "=";
+
+            //combine string variable to make it easy for result
+            int result = num1 + num2;
+            string equation = $"{number1} {sign} {number2} {equalSign} {result}";
+
+            //display the equation with label "sign"
+            lblEquation.Content = equation;
+        }
+        private void btnSubtract_Click(object sender, RoutedEventArgs e)
+        {
+            //create string variables
+            string number1 = txtNumber1.Text;
+            string number2 = txtNumber2.Text;
+
+            //create new variables to parse to make it into math 
+            int num1 = int.Parse(number1);
+            int num2 = int.Parse(number2);
+
+            //create signs for readablity
+            string sign = "-";
+            string equalSign = "=";
+
+            //combine string variable to make it easy for result
+            int result = num1 - num2;
+            string equation = $"{number1} {sign} {number2} {equalSign} {result}";
+
+            //display the equation with label "sign"
+            lblEquation.Content = equation;
+        }
     }
 }
