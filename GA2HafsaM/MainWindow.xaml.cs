31d5887d56ec999d27ece2d76e60395571cdc5b2
hyktv <hafsa.yktv@gmail.com>
2023-10-16 12:44:35 -0700

    Add project files.

---
+++ GA2HafsaM/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GA2HafsaM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Display Full Name
+        private void btnFullName_Click(object sender, RoutedEventArgs e)
+        {
+            string firstName = txtFirstName.Text;
+            string lastName = txtLastName.Text;
+
+            string fullName = $"{firstName} {lastName}";
+
+            MessageBox.Show(fullName);
+        } //btnFullName_Click
+
+
+    }
+}
